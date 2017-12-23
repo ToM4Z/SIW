0622966eadac044b490acfa022ba4ed1c18507b9
ToM4Z <thom_96@hotmail.it>
2017-12-23 19:46:53 +0100

    Removed space and comments from DaoFactory

--- SocialNetwork/src/persistence/DAOFactory.java
+++ SocialNetwork/src/persistence/DAOFactory.java
@@ -5,22 +5,9 @@ import persistence.dao.*;
 
 public abstract class DAOFactory {
 	
-	// --- List of supported DAO types ---
-
-	
-		/**
-		 * Numeric constant '1' corresponds to explicit Hsqldb choice
-		 */
 		public static final int HSQLDB = 1;
-		
-		/**
-		 * Numeric constant '2' corresponds to explicit Postgres choice
-		 */
 		public static final int POSTGRESQL = 2;
-		
-		
-		// --- Actual factory method ---
-		
+				
 		/**
 		 * Depending on the input parameter
 		 * this method returns one out of several possible 
@@ -37,9 +24,6 @@ public abstract class DAOFactory {
 				return null;
 			}
 		}
-		
-		
-		
 		// --- Factory specification: concrete factories implementing this spec must provide this methods! ---
 		
 		/**
